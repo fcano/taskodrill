0739eec37db5f86711b17cc7ddf15531a56119f6
Florencio Cano <florencio.cano@gmail.com>
2020-07-20 20:38:22 +0200

    Add myauth test

--- myauth/tests.py
+++ myauth/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class MyUserViews(TestCase):
+    def test_signup_view(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Password confirmation")
