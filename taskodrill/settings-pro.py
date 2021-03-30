613e6d6edef310ed78040e0d699ac2489d0b2dd9
Florencio Cano <fcanogab@redhat.com>
2021-03-30 11:50:15 +0200

    Remove config from prod settings

--- taskodrill/settings-pro.py
+++ taskodrill/settings-pro.py
@@ -95,9 +95,9 @@ WSGI_APPLICATION = 'taskodrill.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '***REMOVED***',
-        'USER': '***REMOVED***',
-        'PASSWORD': '***REMOVED***',
+        'NAME': os.environ['DBNAME'],
+        'USER': os.environ['DBUSER'],
+        'PASSWORD': os.environ['DBPASS'],
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
