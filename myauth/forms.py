e4438a7f0da068d095c0e3785df0665a68e18f31
Florencio Cano <florencio.cano@gmail.com>
2021-03-31 11:20:54 +0200

    Add Sign Up link to login; Remove text hints from Sign Up form

--- myauth/forms.py
+++ myauth/forms.py
@@ -2,8 +2,14 @@ from django.contrib.auth.forms import UserCreationForm
 
 from .models import MyUser
 
+
 class SignUpForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ["username", "password1", "password2"]:
+            self.fields[fieldname].help_text = None
 
     class Meta:
         model = MyUser
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ("username", "email", "password1", "password2")
