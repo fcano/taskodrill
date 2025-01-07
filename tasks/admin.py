05784b563d316c343fb95247720d95747b56b684
Florencio Cano <florencio.cano@gmail.com>
2025-01-07 10:33:11 +0100

    Add Tasks searchable in admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -6,6 +6,9 @@ from django.contrib.auth.admin import UserAdmin
 class MyUserAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ('task_count',)
 
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
 admin.site.register(MyUser, MyUserAdmin)
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
 admin.site.register(Project)
